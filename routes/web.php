f9d76af9c3716793b0f8ae9ca5c471bd258d2b17
EinsteinYap <einsteinyap2014@gmail.com>
2023-10-08 00:56:33 +0800

    UML diagram added, User and Vendor frontend basic done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -34,5 +33,7 @@ require __DIR__.'/auth.php';
 Route::get('admin/login',[AdminController::class,'login'])->name('admin.login');
 
 //user frontend
-
+Route::group(['middleware'=>['auth','verified'],'prefix'=>'user','as'=>'user.'],function(){
+    Route::get('dashboard',[UserDashboardController::class,'index'])->name('dashboard');
+});
 
